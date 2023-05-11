db8114388ad64f6daa61304a60f0bd8df43d984e
dp3why <dp3why@gmail.com>
2023-05-11 09:27:35 -0700

    version

--- src/test/java/dp3why/inst/InstApplicationTests.java
+++ src/test/java/dp3why/inst/InstApplicationTests.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class InstApplicationTests {
-
+public class InstApplicationTests {
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }

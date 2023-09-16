57d0263942154d34dc765a283fec0bbe77020eb7
dp3why <dp3why@gmail.com>
2023-09-16 00:29:53 -0700

    cor

---
+++ src/main/java/dp3why/inst/Config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package dp3why.inst.Config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "https://lambent-praline-ebd115.netlify.app",
+                        "http://localhost")
+                .allowedMethods(
+                "GET", "POST", "PUT", "DELETE"
+        );
+    }
+
+
+}
